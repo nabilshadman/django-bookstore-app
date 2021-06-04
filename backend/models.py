8289037a2bcd1181e55794834066bf32dafcc408
Nabil <nabilshadman@users.noreply.github.com>
2021-06-04 18:53:22 +0600

    Add files via upload

---
+++ backend/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Book(models.Model):
+    FICTION = 'FI'
+    NONFICTION = 'NF'
+    SCIENCE = 'SC'
+
+    BOOK_TYPS = [
+        (FICTION,'Fiction'),
+        (NONFICTION,'Non-fiction'),
+        (SCIENCE,'Science')
+    ]
+
+    book_title = models.CharField(max_length=200, unique=True, null=False)
+    authors = models.CharField(max_length=200, null=False)
+    book_type = models.CharField(max_length=2, choices=BOOK_TYPS, default=NONFICTION, null=False)
+    price = models.FloatField(default=0, db_column='the_price', null=False)
+
+    def to_string(self):
+        return self.book_title + ' by ' + self.authors + ' ,$' + str(self.price) + ' (' + self.book_type + ')'
---
+++ backend/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Book
+from .forms import BookForm
+
+def index(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BookForm()
+    return render(request,'backend/index.html',{'form':form})
